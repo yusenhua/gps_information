28242f907505762e3314d6d997fd4b2bc156bb27
yusenhua@lvmama.com <yusenhua@lvmama.com>
2017-09-28 13:41:13 +0800

    sdf

---
+++ src/main/java/com/gps/information/GpsInformactApplication.java
@@ -0,0 +1,12 @@
+package com.gps.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsInformactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsInformactApplication.class, args);
+	}
+}
